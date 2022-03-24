4fe402ed9c0e980bcaea8da45db0c54cb83919b7
ProgrammingMuffin <deepakcoder98@gmail.com>
2022-03-24 09:25:49 +0530

    add another pwm pin

--- server/motor.py
+++ server/motor.py
@@ -2,27 +2,34 @@ import RPi.GPIO as GPIO
 
 
 duty_cycle = 0
-pwm = None
+pwm_first = None
+pwm_second = None
 
 def init_gpio():
-    global pwm
+    global pwm_first, pwm_second
     GPIO.setwarnings(True)
     GPIO.setmode(GPIO.BOARD)
     GPIO.setup(12, GPIO.OUT)
-    pwm = GPIO.PWM(12, 100)
+    GPIO.setup(11, GPIO.OUT)
+    pwm_first = GPIO.PWM(12, 100)
+    pwm_second = GPIO.PWM(33, 100)
+    pwm_first.start(0)
+    pwm_second.start(0)
 
 
 def clear_gpio():
-    global pwm
-    pwm.stop()
+    global pwm_first, pwm_second
+    pwm_first.stop()
+    pwm_second.stop()
     GPIO.cleanup()
 
 def change_duty_cycle(duty):
-    global pwm, duty_cycle
+    global pwm_first, pwm_second, duty_cycle
     response = dict()
     prev_duty_cycle = duty_cycle
     duty_cycle = duty
-    pwm.ChangeDutyCycle(duty_cycle)
+    pwm_first.ChangeDutyCycle(duty_cycle)
+    pwm_second.ChangeDutyCycle(duty_cycle)
     response['prev_duty_cycle'] = prev_duty_cycle
     response['duty_cycle'] = duty_cycle
     return response
